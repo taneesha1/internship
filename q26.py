75a7931e9441cef94281bb35aab20b8a5e8802b7
taneesha1 <144047798+taneesha1@users.noreply.github.com>
2024-06-20 00:36:10 +0530

    Create q26.py

---
+++ q26.py
@@ -0,0 +1,18 @@
+str1="this is the main statement"
+
+x=str1.startswith("this")
+
+if x==True:
+    print("this is a prefix")
+
+else:
+    print("not a prefix")
+
+
+y= str1.endswith("statement")
+
+if y==True:
+         print("statement is a suffix")
+
+else:
+     print("not a suffix")
